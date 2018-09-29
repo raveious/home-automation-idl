d9ab3452fa5e5151d07a9988ca27ef09813190cf
Ian Wakely <raveious.irw@gmail.com>
2018-09-29 17:58:22 -0400

    Adding project name into the path.

--- cmake/modules/IDLGeneration.cmake
+++ cmake/modules/IDLGeneration.cmake
@@ -12,25 +12,24 @@ function(fastrtpsgen idl_files output_files)
     set(cpp_file_types ${idl_name}PubSubTypes.cxx)
     set(header_file_types ${idl_name}PubSubTypes.h)
 
-    add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/generated/src/${cpp_file}
-                              ${PROJECT_BINARY_DIR}/generated/src/${cpp_file_types}
-                              ${PROJECT_BINARY_DIR}/generated/include/${header_file}
-                              ${PROJECT_BINARY_DIR}/generated/include/${header_file_types}
-        COMMAND mkdir -p ${PROJECT_BINARY_DIR}/generated/include
-        COMMAND mkdir -p ${PROJECT_BINARY_DIR}/generated/src
-        COMMAND ${FASTRTPSGEN} -d ${PROJECT_BINARY_DIR}/generated/include -replace ${idl_file}
-        COMMAND mv ${PROJECT_BINARY_DIR}/generated/include/*.cxx ${PROJECT_BINARY_DIR}/generated/src/
+    add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/src/${cpp_file}
+                              ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/src/${cpp_file_types}
+                              ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/include/${header_file}
+                              ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/include/${header_file_types}
+        COMMAND mkdir -p ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/include ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/src
+        COMMAND ${FASTRTPSGEN} -d ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/include -replace ${idl_file}
+        COMMAND mv ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/include/*.cxx ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/src/
         COMMENT "Generating source from ${idl_file}"
         DEPENDS ${idl_file}
     )
 
-    list(APPEND ${output_files} ${PROJECT_BINARY_DIR}/generated/src/${cpp_file}
-                                ${PROJECT_BINARY_DIR}/generated/src/${cpp_file_types}
-                                ${PROJECT_BINARY_DIR}/generated/include/${header_file}
-                                ${PROJECT_BINARY_DIR}/generated/include/${header_file_types})
+    list(APPEND ${output_files} ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/src/${cpp_file}
+                                ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/src/${cpp_file_types}
+                                ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/include/${header_file}
+                                ${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/include/${header_file_types})
 
     set(${output_files} ${${output_files}} PARENT_SCOPE)
-    include_directories(${PROJECT_BINARY_DIR}/generated/include)
+    include_directories(${PROJECT_BINARY_DIR}/generated/${PROJECT_NAME}/include)
   endforeach(idl_file)
 endfunction(fastrtpsgen)
 endif(FASTRTPSGEN)
